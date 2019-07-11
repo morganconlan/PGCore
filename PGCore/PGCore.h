98a9bd1b1e32d459db8aee382bdee5d1995ea0b5
Morgan Conlan <morgan@pangr.ie>
2019-07-11 17:17:44 +0100

    Initial Commit

---
+++ PGCore/PGCore.h
@@ -0,0 +1,19 @@
+//
+//  PGCore.h
+//  PGCore
+//
+//  Created by Morgan Conlan on 11/07/2019.
+//  Copyright © 2019 Morgan Conlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGCore.
+FOUNDATION_EXPORT double PGCoreVersionNumber;
+
+//! Project version string for PGCore.
+FOUNDATION_EXPORT const unsigned char PGCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGCore/PublicHeader.h>
+
+
---
+++ PGCoreTests/PGCoreTests.m
@@ -0,0 +1,37 @@
+//
+//  PGCoreTests.m
+//  PGCoreTests
+//
+//  Created by Morgan Conlan on 11/07/2019.
+//  Copyright © 2019 Morgan Conlan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PGCoreTests : XCTestCase
+
+@end
+
+@implementation PGCoreTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
